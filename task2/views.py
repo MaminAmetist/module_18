b303622544e385bc0547781f2d64f34e2721cd62
Anna <anna.nezdulkina@mail.ru>
2024-12-04 21:03:40 +0700

    one commit

--- task2/views.py
+++ task2/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+
 # Create your views here.
 def start_func(request):
     return render(request, 'temple_start.html')
 
+
 def first_func(request):
     return render(request, 'temple_func.html')
+
 
 class first_class(TemplateView):
     template_name = 'temple_class.html'
