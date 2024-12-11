9a62468e9b6ca9d9ce38a6a3f46d384020273bea
Anna <anna.nezdulkina@mail.ru>
2024-12-11 21:05:45 +0700

    HTML и Django

--- task5/forms.py
+++ task5/forms.py
@@ -1,9 +1,16 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+def validate_age(value):
+    if int(value) < 18:
+        raise ValidationError('Вы должны быть старше 18.')
 
 
 class UserRegister(forms.Form):
     username = forms.CharField(max_length=30, label='Введите логин:')
     password = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Введите пароль:')
     repeat_password = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Повторите пароль:')
-    age = forms.IntegerField(max_value=999, min_value=18, label='Введите свой возраст:')
+    age = forms.IntegerField(min_value=18, label='Введите свой возраст:', validators=[validate_age])
+    # age = forms.IntegerField(label='Введите свой возраст:', validators=[validate_age])
     subscribe = forms.BooleanField(required=False, label='Подписаться на рассылку.')
