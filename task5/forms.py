707a3d0d0f2638303a1f492264a22434e279fb21
Anna <anna.nezdulkina@mail.ru>
2024-12-12 21:45:17 +0700

    HTML и Django

--- task5/forms.py
+++ task5/forms.py
@@ -10,13 +10,13 @@ def validate_age(value):
 
 class UserRegister(forms.Form):
     username = forms.CharField(max_length=30, label='Введите логин:', error_messages={'required': 'Тысяча чертей!', },
-                               help_text='Имя пользователя должно быть не более 30 символов.')
+                               help_text='Имя пользователя должно содержать не более 30 символов.')
     password = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Введите пароль:',
                                error_messages={'required': 'Пароль слишком короткий.', },
                                help_text='Пароль должен содержать не менее 8 символов.')
     repeat_password = forms.CharField(min_length=8, widget=forms.PasswordInput(), label='Повторите пароль:')
     age = forms.IntegerField(min_value=18, label='Введите свой возраст:', validators=[validate_age],
-                             error_messages={'required': 'Вы должны быть старше 18.', })
+                             error_messages={'required': 'Вы должны быть старше 18.', }, )
     subscribe = forms.BooleanField(required=False, label='Подписаться на рассылку.')
 
     def clean_username(self):
@@ -25,17 +25,21 @@ class UserRegister(forms.Form):
         if ' ' in username:
             raise forms.ValidationError('Имя пользователя не должно содержать пробелы.')
         elif len(username) > 30:
-            raise forms.ValidationError('Имя пользователя должно быть не более 30 символов.')
+            raise forms.ValidationError('Имя пользователя должно содержать не более 30 символов.')
         elif username in users:
             raise forms.ValidationError('Пользователь уже существует.')
         return username
+
+    def clean_repeat_password(self):
+        repeat_password = self.repeat_password
+        return repeat_password
 
     def clean_password(self):
         password = self.cleaned_data.get('password')
         repeat_password = self.cleaned_data.get('repeat_password')
         if password != repeat_password:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return password
+            raise forms.ValidationError(f'Пароли не совпадают.{password, repeat_password}')
+        return password, repeat_password
 
     def clean_age(self):
         age = self.cleaned_data.get('age')
